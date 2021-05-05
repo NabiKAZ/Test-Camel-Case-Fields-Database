ff6a2f0579d4543a7f197a098f803a956a91d02d
Nabi <nabikaz@gmail.com>
2021-05-05 18:43:45 +0430

    just success to passed

--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,5 @@
-<p>id: <strong><?= $model->id ?> &nbsp;—&nbsp;  (<?= (isset($model->id) ? '<span style="color:green">Success</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
-<p>email: <strong><?= $model->email ?> &nbsp;—&nbsp;  (<?= (isset($model->email) ? '<span style="color:green">Success</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
-<p>user_name: <strong><?= $model->user_name ?> &nbsp;—&nbsp;  (<?= (isset($model->user_name) ? '<span style="color:green">Success</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
-<p>fullName: <strong><?= $model->fullName ?> &nbsp;—&nbsp;  (<?= (isset($model->fullName) ? '<span style="color:green">Success</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
-<p>Tel_Number: <strong><?= $model->Tel_Number ?> &nbsp;—&nbsp;  (<?= (isset($model->Tel_Number) ? '<span style="color:green">Success</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
+<p>id: <strong><?= $model->id ?> &nbsp;—&nbsp;  (<?= (isset($model->id) ? '<span style="color:green">Passed</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
+<p>email: <strong><?= $model->email ?> &nbsp;—&nbsp;  (<?= (isset($model->email) ? '<span style="color:green">Passed</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
+<p>user_name: <strong><?= $model->user_name ?> &nbsp;—&nbsp;  (<?= (isset($model->user_name) ? '<span style="color:green">Passed</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
+<p>fullName: <strong><?= $model->fullName ?> &nbsp;—&nbsp;  (<?= (isset($model->fullName) ? '<span style="color:green">Passed</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
+<p>Tel_Number: <strong><?= $model->Tel_Number ?> &nbsp;—&nbsp;  (<?= (isset($model->Tel_Number) ? '<span style="color:green">Passed</span>' : '<span style="color:red">Failed</span>') ?>)</span></p>
