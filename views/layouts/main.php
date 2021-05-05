867950fa2fd647eacc84885e847e6543a06a51f2
Nabi <nabikaz@gmail.com>
2021-05-05 18:41:00 +0430

    improve results
    Signed-off-by: Nabi <nabikaz@gmail.com>

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,7 @@
 <h2>Test Camel Case Fields Database For Yii2</h2>
 <h3>OS: <?= PHP_OS ?></h3>
-<h5><?= php_uname() ?></h5>
+<h4>Uname: <?= php_uname() ?></h4>
+<h4>PHP: <?= phpversion() ?></h4>
+<h4>MySQL: <?= \Yii::$app->db->getSchema()->getServerVersion() ?></h4>
 <hr>
 <?= $content ?>
